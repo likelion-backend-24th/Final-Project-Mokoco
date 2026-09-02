972dc6434ceed4c11db4f29f17cbc8f78854350f
jun <wnstjd117@gmail.com>
2026-09-02 16:14:04 +0900

    fix: api 명세서에 맞춰 수정

--- backend/user-service/src/main/java/com/team2/userservice/region/controller/RegionController.java
+++ backend/user-service/src/main/java/com/team2/userservice/region/controller/RegionController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/users/region")
+@RequestMapping("/api/users/me/region")
 public class RegionController {
 
     private final RegionService regionService;
