747717c0af85aa3d17279e2bab880862ced1b46c
jun <wnstjd117@gmail.com>
2026-09-08 13:55:40 +0900

    feat/chat 채팅방 조회 api 추가

--- backend/post-service/src/main/java/com/team2/postservice/chatRoom/controller/ChatRoomController.java
+++ backend/post-service/src/main/java/com/team2/postservice/chatRoom/controller/ChatRoomController.java
@@ -5,10 +5,7 @@ import com.team2.postservice.chatRoom.service.ChatRoomService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/chat-rooms")
@@ -25,5 +22,15 @@ public class ChatRoomController {
         ChatRoomResponse response = chatRoomService.createChatRoom(fixDealId, email);
 
         return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/fix-deals/{fixDealId}")
+    public ResponseEntity<ChatRoomResponse> getChatRoom(
+            @PathVariable Long fixDealId,
+            @AuthenticationPrincipal String email
+    ) {
+        return ResponseEntity.ok(
+                chatRoomService.getChatRoom(fixDealId, email)
+        );
     }
 }
