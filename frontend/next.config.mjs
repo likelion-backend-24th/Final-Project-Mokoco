723cd849f2c1820aacd27999ca839bd5ae6e66a5
jake1031 <jake1031@naver.com>
2026-09-09 16:26:31 +0900

    fix: docker-compose파일 수정v3

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone', // 이 설정이 있어야 빌드 폴더에 server.js가 생성됩니다.
+  output: 'standalone',
 }
 
-module.exports = nextConfig
+export default nextConfig
