670935b9f840a29092822acf6a48df76cd1e6ee8
jake1031 <jake1031@naver.com>
2026-09-09 23:36:34 +0900

    fix: OAuth 로그인 기능 수정

--- frontend/src/components/login-form.js
+++ frontend/src/components/login-form.js
@@ -34,7 +34,7 @@ export default function LoginForm({ registered = false }) {
   }
 
   const handleSocialLogin = (provider) => {
-    const backendBaseUrl = process.env.NEXT_PUBLIC_BACKEND_API_URL || "http://localhost:8000";
+    const backendBaseUrl = process.env.NEXT_PUBLIC_BACKEND_API_URL || "http://18.212.91.33:8081";
     window.location.href = `${backendBaseUrl}/oauth2/authorization/${provider}`;
   };
 
