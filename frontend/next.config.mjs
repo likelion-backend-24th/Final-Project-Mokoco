08eeddb506e95a3bcc782fd6c1d6980043203f3c
jake1031 <jake1031@naver.com>
2026-09-09 15:36:49 +0900

    chore: release 브랜치용 ci/cd 파이프라인 설정

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'standalone', // 이 설정이 있어야 빌드 폴더에 server.js가 생성됩니다.
+}
 
-export default nextConfig;
+module.exports = nextConfig
