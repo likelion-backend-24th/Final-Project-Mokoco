7a0bbd1b719ca046e1afe95f0fba769b07cf82f4
jake1031 <jake1031@naver.com>
2026-09-10 00:16:37 +0900

    fix: OAuth 로그인 기능 수정v3

--- frontend/src/components/login-form.js
+++ frontend/src/components/login-form.js
@@ -35,7 +35,7 @@ export default function LoginForm({ registered = false }) {
 
   const handleSocialLogin = (provider) => {
     const domain = provider === "google" ? "18.212.91.33.nip.io" : "18.212.91.33";
-    window.location.href = `http://${domain}:8081/oauth2/authorization/${provider}`;
+    window.location.href = `http://${domain}:8000/oauth2/authorization/${provider}`;
   };
 
   return (
